ac86be393960b55bace56e0a1c9e4630bb3c587f
evghenii <volkov.jenea@gmail.com>
2018-09-21 11:36:08 +0300

    bookmarking of nodes

--- web/modules/custom/custom_block/src/Controller/CustomController.php
+++ web/modules/custom/custom_block/src/Controller/CustomController.php
@@ -2,15 +2,24 @@
 
 namespace Drupal\custom_block\Controller;
 
+use Drupal\redirect\Entity\Redirect;
 use Drupal\Core\Controller\ControllerBase;
-
+use Drupal\user\Entity\User;
 
 class CustomController extends ControllerBase {
 
-  public function content() {
-    $uid = \Drupal::currentUser()->id();
-    return array();
+  public function content($node, $uid) {
+    $user = \Drupal\user\Entity\User::load($uid);
+    $user->field_user_bookmark[] = ['target_id' => $node];
 
+    $user->save();
+
+    return array();
   }
 
+  function my_goto (){
+    g
+    $redirect->setSource('old-page.php', array('id' => 107));
+    $redirect->save();
+  }
 }
